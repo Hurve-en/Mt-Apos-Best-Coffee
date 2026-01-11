11b32e720a0609d3f2cbde002b75e5db3be9883f
Hurve-en <142562684+Hurve-en@users.noreply.github.com>
2026-01-11 20:41:25 +0800

    Fixed Navigation Bar

--- coffee-app/frontend/src/components/Common/Navbar.tsx
+++ coffee-app/frontend/src/components/Common/Navbar.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { useAppDispatch, useAppSelector } from '../hooks/useRedux';
-import { logout } from '../redux/slices/authSlice';
+import { useAppDispatch, useAppSelector } from '../../hooks/useRedux';
+import { logout } from '../../redux/slices/authSlice';
 
 const Navbar: React.FC = () => {
   const navigate = useNavigate();
